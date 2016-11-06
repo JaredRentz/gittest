1057201e336cff23467580584d5bc5bbb96af1a0
Jr6Over6 <Jared@Jared-2.local>
2016-11-06 16:38:50 -0500

    Going to File

--- Tuples.py
+++ Tuples.py
@@ -1,1 +1,17 @@
-Tuples.py
+''' 
+Similar to list but they can be immutable
+'''
+l = (1,2,3,4)
+# Get the count of the tuples. Similar to a List!
+#print(len(l))
+
+# Grab the last number in the Tuple
+#print(l[:-1])
+
+# See how many times something appears in a Tuple
+
+t = (1,2,4,3,2,2,1,2,3,2,1)
+
+print(t.count(1))
+num = 2
+print("%s appears %s times in the Tuple"%(t.count(2)))
